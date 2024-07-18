ff124c8d2fd790ab43e9a18434944453e3b1c5f0
Bryan Garcia <bags@Bryans-Air.localdomain>
2024-07-18 14:24:32 -0400

    added files

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,23 +1,25 @@
 export default class Currency {
   constructor(name, code) {
-    this._name = name;
     this._code = code;
-  }
-
-  get name() {
-    return this._name;
+    this._name = name;
   }
 
   get code() {
     return this._code;
   }
 
-  set name(name) {
-    this._name = name;
+  get name() {
+    return this._name;
   }
 
   set code(code) {
+    if (typeof code !== 'string') throw new TypeError('Code must be a string');
     this._code = code;
+  }
+
+  set name(name) {
+    if (typeof name !== 'string') throw new TypeError('Name must be a string');
+    this._name = name;
   }
 
   displayFullCurrency() {
