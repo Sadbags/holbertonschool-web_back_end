23ce8379575df0b4e344e37b771acc4c24883a8d
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2024-07-18 18:06:44 -0400

    added files

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,10 +1,8 @@
 export default class Building {
   constructor(sqft) {
-    if (typeof sqft !== 'number') throw new TypeError('Square footage must be a number');
     this._sqft = sqft;
-
-    if (new.target === Building) {
-      throw new Error('Building is an abstract class and cannot be instantiated directly.');
+    if (this.constructor.name !== 'Building') {
+      this.evacuationWarningMessage();
     }
   }
 
@@ -12,19 +10,11 @@ export default class Building {
     return this._sqft;
   }
 
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
-  }
-}
-
-// Example subclass
-class OfficeBuilding extends Building {
-  constructor(sqft, name) {
-    super(sqft);
-    this._name = name;
+  set sqft(sqft) {
+    this._sqft = sqft;
   }
 
   evacuationWarningMessage() {
-    return `Evacuate the ${this._name} building immediately!`;
+    throw Error('Class extending Building must override evacuationWarningMessage');
   }
 }
