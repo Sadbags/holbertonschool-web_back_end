41abea4ec66918ede085692c541dfe618bbe159a
Bryan Garcia <bags@Bryans-Air.localdomain>
2024-07-18 13:27:00 -0400

    added files

---
+++ ES6_classes/1-make_classrooms.js
@@ -0,0 +1,10 @@
+import classRoom from './0-classroom';
+
+function initializedRooms() {
+	return [
+		new classRoom(19),
+		new classRoom(20),
+		new classRoom(34)
+	]
+}
+export default initializedRooms
