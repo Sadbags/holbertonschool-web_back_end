1cc9e27849c92f10d363a8aa79286b92ef19174e
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2024-07-23 16:35:26 -0400

    added file

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,6 +1,6 @@
 export default function createInt8TypedArray(length, position, value) {
-	const buffer = new ArrayBuffer(length);
-	const view = new DataView(buffer);
+  const buffer = new ArrayBuffer(length);
+  const view = new DataView(buffer);
 
   if (position < 0 || position >= length) {
   throw new Error('Position outside range');
