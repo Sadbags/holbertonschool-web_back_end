c5ed33576f050939180aeb01ecc7808a90d81555
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2024-08-13 11:34:35 -0400

    added readme and first file

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+""" type-annotated function that takes a float a and a float b as arguments and returns their sum as a float. """
+
+def add(a: float, b: float) -> float:
+    """ add func """
+    return a + b
