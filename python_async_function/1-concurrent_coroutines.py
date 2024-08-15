08bcc122db45e497a5aae9df97ccc1c592021c26
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2024-08-15 12:34:51 -0400

    added files

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -15,8 +15,8 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """ espera """
+    """ Wait_n """
     times: List[float] = []
     for i in range(n):
-        times.append(await wait_random(max_delay))
+        times.append(wait_random(max_delay))
     return [await i for i in asyncio.as_completed(times)]
