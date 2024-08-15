923eb75a4038406899498e9ac6bf235dbeaabde9
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2024-08-15 12:23:59 -0400

    added files

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -9,6 +9,7 @@ seconds and eventually returns it. """
 import asyncio
 import random
 
+
 async def wait_random(max_delay: int = 10) -> float:
     """ wait random """
     delay: float = random.uniform(0, max_delay)
