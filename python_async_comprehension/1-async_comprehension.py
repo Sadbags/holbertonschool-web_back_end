11f9ae4ce0c015628605ce5648f0c5e882ef6619
Bryan <bgarcia195@gmail.com>
2024-08-19 10:16:59 -0400

    added file

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -10,5 +10,5 @@ async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_generator() -> List[float]:
-	""" Async_comprehension """
-	return [n async for n in async_generator()]
+    """ Async_comprehension """
+    return [n async for n in async_generator()]
